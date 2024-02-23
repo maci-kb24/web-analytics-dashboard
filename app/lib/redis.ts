55a1b886e0ae69668b5c16982c21c131c8b5578b
macikb8@yahoo.com <macikb8@yahoo.com>
2024-02-23 15:17:33 +0100

    setup redis database and buld the analytics logic

---
+++ app/lib/redis.ts
@@ -0,0 +1,8 @@
+import { Redis } from '@upstash/redis'
+
+export const redis = new Redis({
+    url: 'https://eu1-stirred-platypus-39526.upstash.io',
+    token: process.env.REDIS_KEY!,
+})
+
+const data = await redis.set('foo', 'bar');
