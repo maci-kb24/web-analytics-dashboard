3b56d6542f3fe9f2895501ac685712eb1ae9f89e
macikb8@yahoo.com <macikb8@yahoo.com>
2024-02-28 20:46:17 +0100

    change quotes in middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -4,12 +4,12 @@ import { analytics } from "./utils/analytics";
 export default async function middleware(req: NextRequest) {
     if(req.nextUrl.pathname === "/") {
         try {
-        await analytics.track("pageview", { 
-            page: "/",
+        await analytics.track('pageview', { 
+            page: '/',
             country: req.geo?.country,
-         });       
-        } catch (error) {
-            console.error(error);
+         })       
+        } catch (err) {
+            console.error(err);
         }
     }
 
